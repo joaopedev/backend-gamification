abba8fa5a23035ff4bf4da34b3891525bafd9a6b
joaopedev <joaopedev@outlook.com>
2025-03-31 15:50:53 -0300

    Connect db local;

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { StickersModule } from './stickers/stickers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, StickersModule],
+  imports: [UsersModule, StickersModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    password: 'postgres',
+    port: 5432,
+    username: 'postgres',
+    database: 'dbgamification',
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
