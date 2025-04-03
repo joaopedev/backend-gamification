343943b8541e8d9824a544149324669117eb070f
joaopedev <joaopedev@outlook.com>
2025-04-02 22:38:48 -0300

    New params for database and new Controllers.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { StickersModule } from './stickers/stickers.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TradesModule } from './trades/trades.module';
+import { StickerPackModule } from './sticker-pack/sticker-pack.module';
+import { TasksModule } from './tasks/tasks.module';
+import { UserStickersModule } from './user-stickers/user-stickers.module';
+import { CoinTransactionModule } from './coin-transaction/coin-transaction.module';
 
 @Module({
   imports: [UsersModule, StickersModule, TypeOrmModule.forRoot({
@@ -13,7 +18,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     port: 5432,
     username: 'postgres',
     database: 'dbgamification',
-  }),],
+    logging: true,
+    synchronize: true,
+  }), TradesModule, StickerPackModule, TasksModule, UserStickersModule, CoinTransactionModule,],
   controllers: [AppController],
   providers: [AppService],
 })
