b70837d70a99d8b916a4e377b0e59b9481a89aea
joaopedev <joaopedev@outlook.com>
2025-04-30 19:09:41 -0300

    Modificações do service de criação de usuarios;

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { User } from './entities/user.entity';
+import { Users } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { StickerPack } from 'src/sticker-pack/entities/sticker-pack.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([Users, StickerPack])],
   controllers: [UsersController],
   providers: [UsersService],
 })
