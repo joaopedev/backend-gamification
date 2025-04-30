706e82636c802e5f7d61561c3adfd39d1eeef40c
joaopedev <joaopedev@outlook.com>
2025-04-30 19:10:25 -0300

    Sticker module

--- src/stickers/entities/sticker.entity.ts
+++ src/stickers/entities/sticker.entity.ts
@@ -1,4 +1,5 @@
-import { User } from 'src/users/entities/user.entity';
+import { StickerPack } from 'src/sticker-pack/entities/sticker-pack.entity';
+import { Users } from 'src/users/entities/user.entity';
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('stickers')
@@ -12,9 +13,27 @@ export class Sticker {
   @Column({ type: 'varchar', length: 100 })
   image_url: string;
 
-  @ManyToOne(() => User)
-  user: User;
+  @ManyToOne(() => Users)
+  user: Users;
 
   @Column({ type: 'varchar', length: 100 })
   sponsor: string; // Number of stickers in the collection
+
+  @Column({type: 'boolean', default: false})
+  pasted: boolean; // Indicates if the sticker pack is pasted or not
+
+  @Column({ type: 'varchar', length: 100 })
+  description: string;
+
+  @ManyToOne(() => StickerPack, (stickerPack) => stickerPack.stickers)
+  stickerPack: StickerPack[];
+
+  @Column({ type: 'varchar', length: 100 })
+  category: string; // Category of the sticker
+
+  @Column({ type: 'varchar', length: 100 })
+  area: string; // Area of the sticker
+
+  @Column({ type: 'varchar', length: 100 })
+  sub_category?: string; // Area of the sticker
 }
--- src/stickers/stickers.module.ts
+++ src/stickers/stickers.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { StickersService } from './stickers.service';
 import { StickersController } from './stickers.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/users/entities/user.entity';
+import { Users } from 'src/users/entities/user.entity';
 import { Sticker } from './entities/sticker.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sticker,  User])],
+  imports: [TypeOrmModule.forFeature([Users, Sticker])],
   controllers: [StickersController],
   providers: [StickersService],
 })
