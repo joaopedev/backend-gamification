b4284fadbfa8daae8650c97674eef8265ff67e41
joaopedev <joaopedev@outlook.com>
2025-05-22 00:20:15 -0300

    Rota para solicitações recebidas do usuario e historico de solicitações de status pendente;

--- src/trades/trades.controller.ts
+++ src/trades/trades.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TradesService } from './trades.service';
 import { CreateTradeDTO } from './dto/create-trade.dto';
 import { UpdateTradeDto } from './dto/update-trade.dto';
@@ -25,6 +33,16 @@ export class TradesController {
   @Get('user/:userId')
   findAllTradesByUserId(@Param('userId') userId: string) {
     return this.tradesService.findAllTradesByUserId(+userId);
+  }
+
+  @Get('user/:userId/received')
+  findAllReceivedTradesByUserId(@Param('userId') userId: string) {
+    return this.tradesService.findAllReceivedTradesByUserId(+userId);
+  }
+
+  @Get('user/:userId/history')
+  findTradeHistoryByUserId(@Param('userId') userId: string) {
+    return this.tradesService.findTradeHistoryByUserId(+userId);
   }
 
   @Patch(':id')
