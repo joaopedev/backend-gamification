bd6ae36de55a6b81d4b78462cfb6283098573684
joaopedev <joaopedev@outlook.com>
2025-05-21 22:15:40 -0300

    Adicionado modulo users para utilização em relationships;

--- src/friends-relationship/friends-relationship.module.ts
+++ src/friends-relationship/friends-relationship.module.ts
@@ -3,9 +3,10 @@ import { FriendsRelationshipService } from './friends-relationship.service';
 import { FriendsRelationshipController } from './friends-relationship.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FriendsRelationship } from './entities/friends-relationship.entity';
+import { Users } from 'src/users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FriendsRelationship])],
+  imports: [TypeOrmModule.forFeature([FriendsRelationship, Users])],
   controllers: [FriendsRelationshipController],
   providers: [FriendsRelationshipService],
 })
