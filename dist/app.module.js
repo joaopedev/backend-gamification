dec33adb383bac676be57d2e00e9c8c9fc568960
joaopedev <joaopedev@outlook.com>
2025-06-15 21:58:29 -0300

    Fix envio de email;

--- dist/app.module.js
+++ dist/app.module.js
@@ -69,7 +69,7 @@ exports.AppModule = AppModule = __decorate([
                     from: '"Gammification" <gammification@gmail.com>',
                 },
                 template: {
-                    dir: (0, path_1.join)(process.cwd(), 'dist', 'mail', 'templates', 'mail', 'templates'),
+                    dir: (0, path_1.join)(process.cwd(), 'dist', 'mail', 'templates'),
                     adapter: new handlebars_adapter_1.HandlebarsAdapter(),
                     options: {
                         strict: true,
